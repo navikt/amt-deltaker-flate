81b6f45424bccbd2421bc079b689e0e531122dbf
Magnus Evensberget <magnus.evensberget@gmail.com>
2023-10-17 11:08:48 +0200

    Oppdaterte Vite config, la til docker-compose fil.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: resolve(__dirname, 'src/Mikrofrontend.tsx'),
+      preserveEntrySignatures: 'exports-only',
+      output: {
+        entryFileNames: 'bundle.js',
+        format: 'es'
+      }
+    }
+  },
 })
