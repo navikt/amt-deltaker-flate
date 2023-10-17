5e34bfbe10f8441f482bd90a101c21053aeb2ee9
Marthe Frogner Holm <marthe@Marthe-sin-MBP.localdomain>
2023-10-17 11:56:52 +0200

    fix tester

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: resolve(__dirname, 'src/Mikrofrontend.tsx'),
-      preserveEntrySignatures: 'exports-only',
-      output: {
-        entryFileNames: 'bundle.js',
-        format: 'es'
-      }
-    }
-  },
+	plugins: [react()],
+	build: {
+		manifest: true,
+		rollupOptions: {
+			input: resolve(__dirname, 'src/Mikrofrontend.tsx'),
+			preserveEntrySignatures: 'exports-only',
+			output: {
+				entryFileNames: 'bundle.js',
+				format: 'es'
+			}
+		}
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: ['./vitest.setup.ts']
+	}
 })
