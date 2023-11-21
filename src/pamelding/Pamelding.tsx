480d2240e91ee3541aa8a60339e55d0e277c386f
Marthe Frogner Holm <marthe@Marthe-sin-MBP.localdomain>
2023-11-21 13:54:36 +0100

    bruk tailwind

--- src/pamelding/Pamelding.tsx
+++ src/pamelding/Pamelding.tsx
@@ -19,7 +19,7 @@ export const Pamelding = ({
   bakgrunnsinformasjon
 }: PameldingProps) => {
   return (
-    <VStack gap="8" style={{ background: 'white', padding: '1.5rem' }}>
+    <VStack gap="8" className="p-6 bg-white">
       <Heading level="1" size="medium">
         Meld på {deltakerlisteNavn} hos {arrangorNavn}
       </Heading>
