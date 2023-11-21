e323359575ef255fb85a9d0f4e387a8d1db03d19
Magnus Evensberget <magnus.evensberget@nav.no>
2023-11-21 13:15:35 +0100

    Legge til tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type {Config} from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  screens: {
+    xs: '480px',
+    ss: '620px',
+    sm: '768px',
+    md: '1060px',
+    lg: '1200px',
+    xl: '1700px',
+  },
+  plugins: [],
+}
+
+export default config
