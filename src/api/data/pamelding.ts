400c1b23b681ca1cc267346f5ea906d2e8c1a08f
MartheFH <136322563+MartheFH@users.noreply.github.com>
2023-11-23 09:16:49 +0100

    Legge til deltakelsesmengde (#51)

--- src/api/data/pamelding.ts
+++ src/api/data/pamelding.ts
@@ -1,5 +1,19 @@
 import {z} from 'zod'
 
+export enum Tiltakstype {
+  ARBFORB = 'ARBFORB', // Arbeidsforberedende trening / AFT
+  ARBRRHDAG = 'ARBRRHDAG', // Arbeidsrettet rehabilitering / ARR
+  AVKLARAG = 'AVKLARAG', // Avklaring
+  INDOPPFAG = 'INDOPPFAG', // Oppfølging
+  DIGIOPPARB = 'DIGIOPPARB', // Digitalt oppfølgingstiltak
+  GRUFAGYRKE = 'GRUFAGYRKE', // Fag og yrkesopplæring gruppe / er kurs
+  GRUPPEAMO = 'GRUPPEAMO', // Arbeidsmarkedsopplæring i gruppe / er kurs
+  JOBBK = 'JOBBK', // Jobbklubb / er kurs
+  VASV = 'VASV' // Varig tilrettelagt arbeid (skjermet virksomhet) / VTA
+}
+
+export const tiltakstypeSchema = z.nativeEnum(Tiltakstype)
+
 export const malSchema = z.object({
   visningsTekst: z.string(), // kommer fra valp
   type: z.string(), // Enum/String, kommer fra valp
@@ -10,16 +24,17 @@ export const malSchema = z.object({
 export const deltakerlisteSchema = z.object({
   deltakerlisteId: z.string().uuid(),
   deltakerlisteNavn: z.string(),
-  tiltakstype: z.string(), //Enum?
+  tiltakstype: tiltakstypeSchema,
   arrangorNavn: z.string(),
   oppstartstype: z.string(),
-  mal: z.array(malSchema),
+  mal: z.array(malSchema)
 })
 
 export const pameldingSchema = z.object({
   deltakerId: z.string().uuid(),
-  deltakerliste: deltakerlisteSchema,
+  deltakerliste: deltakerlisteSchema
 })
 
 export type Mal = z.infer<typeof malSchema>
+export type Deltakerliste = z.infer<typeof deltakerlisteSchema>
 export type PameldingResponse = z.infer<typeof pameldingSchema>
--- src/pamelding/Pamelding.tsx
+++ src/pamelding/Pamelding.tsx
@@ -1,34 +1,23 @@
 import { Heading, VStack } from '@navikt/ds-react'
-import { type Mal } from '../api/data/pamelding'
+import { Deltakerliste } from '../api/data/pamelding'
 import { ArrangørInfo } from './ArrangørInfo'
 import { PameldingSkjema } from './PameldingSkjema'
 
 export interface PameldingProps {
-  deltakerlisteNavn: string
-  arrangorNavn: string
-  oppstartsType: string
-  mal: Array<Mal>
-  bakgrunnsinformasjon?: string
+  deltakerliste: Deltakerliste
 }
 
-export const Pamelding = ({
-  deltakerlisteNavn,
-  arrangorNavn,
-  oppstartsType,
-  mal,
-  bakgrunnsinformasjon
-}: PameldingProps) => {
+export const Pamelding = ({ deltakerliste }: PameldingProps) => {
   return (
     <VStack gap="8" className="p-6 bg-white">
       <Heading level="1" size="medium">
-        Meld på {deltakerlisteNavn} hos {arrangorNavn}
+        Meld på {deltakerliste.deltakerlisteNavn} hos {deltakerliste.arrangorNavn}
       </Heading>
-      <ArrangørInfo arrangorNavn={arrangorNavn} oppstartsType={oppstartsType} />
-      <PameldingSkjema
-        deltakerlisteNavn={deltakerlisteNavn}
-        mal={mal}
-        bakgrunnsinformasjon={bakgrunnsinformasjon}
+      <ArrangørInfo
+        arrangorNavn={deltakerliste.arrangorNavn}
+        oppstartsType={deltakerliste.oppstartstype}
       />
+      <PameldingSkjema mal={deltakerliste.mal} tiltakstype={deltakerliste.tiltakstype} />
     </VStack>
   )
 }
