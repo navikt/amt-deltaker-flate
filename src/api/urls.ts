bc915ffe372c204db187b91ade447fcd34c9ebd8
Marthe Frogner Holm <marthe@Marthe-sin-MBP.localdomain>
2023-12-05 14:49:44 +0100

    test set up mock

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -16,9 +16,9 @@ export const getEnvironment = () => {
 type EnvUrl = { development: string; production: string; local: string };
 
 const API_URL: EnvUrl = {
-  local: 'http://localhost:8080',
-  development: 'https://www.dev.nav.no/api/endpoint',
-  production: 'https://www.nav.no/api/endpoint',
+  local: 'http://localhost:3000/api',
+  development: 'https://www.dev.nav.no/api',
+  production: 'https://www.nav.no/api'
 }
 
 export const apiUrl = API_URL[getEnvironment()]
--- src/mocks/setupMocks.ts
+++ src/mocks/setupMocks.ts
@@ -8,37 +8,39 @@ import {sendInnPameldingUtenGodkjenningRequestSchema} from '../api/data/send-inn
 const handler = new MockHandler()
 
 export const worker = setupWorker(
-  http.get('/test', () => HttpResponse.json({id: 'abc'})),
+  http.get('/test', () => HttpResponse.json({ id: 'abc' })),
 
-  http.post('/api/pamelding', async ({request}) => {
-    const response = await request.json()
-      .then(json => pameldingRequestSchema.parse(json))
-      .then(body => handler.createPamelding(body))
+  http.post('/api/deltaker', async ({ request }) => {
+    const response = await request
+      .json()
+      .then((json) => pameldingRequestSchema.parse(json))
+      .then((body) => handler.createPamelding(body))
 
     return response
   }),
 
-  http.delete('/api/pamelding/:deltakerId', async ({params}) => {
-    const {deltakerId} = params
+  http.delete('/api/pamelding/:deltakerId', async ({ params }) => {
+    const { deltakerId } = params
     return handler.deletePamelding(deltakerId as string)
   }),
 
-  http.post('/api/pamelding/:deltakerId', async ({request, params}) => {
-    const {deltakerId} = params
+  http.post('/api/pamelding/:deltakerId', async ({ request, params }) => {
+    const { deltakerId } = params
 
-    const response = await request.json()
-      .then(json => sendInnPameldingRequestSchema.parse(json))
-      .then(body => handler.sendInnPamelding(deltakerId as string, body))
+    const response = await request
+      .json()
+      .then((json) => sendInnPameldingRequestSchema.parse(json))
+      .then((body) => handler.sendInnPamelding(deltakerId as string, body))
 
     return response
   }),
+  http.post('/api/pamelding/:deltakerId/utenGodkjenning', async ({ request, params }) => {
+    const { deltakerId } = params
 
-  http.post('/api/pamelding/:deltakerId/utenGodkjenning', async ({request, params}) => {
-    const {deltakerId} = params
-
-    const response = await request.json()
-      .then(json => sendInnPameldingUtenGodkjenningRequestSchema.parse(json))
-      .then(body => handler.sendInnPameldingUtenGodkjenning(deltakerId as string, body))
+    const response = await request
+      .json()
+      .then((json) => sendInnPameldingUtenGodkjenningRequestSchema.parse(json))
+      .then((body) => handler.sendInnPameldingUtenGodkjenning(deltakerId as string, body))
 
     return response
   })
