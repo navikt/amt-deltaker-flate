16795d926ba7b891063a059f08f6723a93f8eeaf
Magnus Evensberget <magnus.evensberget@nav.no>
2023-12-12 21:43:10 +0100

    Nytt oppsett av testsiden

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,9 +2,10 @@ import {PameldingRequest} from './data/pamelding-request.ts'
 import {PameldingResponse, pameldingSchema} from './data/pamelding.ts'
 import {SendInnPameldingRequest} from './data/send-inn-pamelding-request.ts'
 import {SendInnPameldingUtenGodkjenningRequest} from './data/send-inn-pamelding-uten-godkjenning-request.ts'
+import {apiUrl} from '../utils/environment-utils.ts'
 
 export const checkBackend = async () => {
-  return fetch('/amt-deltaker-bff/internal/health/liveness')
+  return fetch(`${apiUrl()}amt-deltaker-bff/internal/health/liveness`)
     .then(response => {
       if (response.status !== 200) {
         throw new Error(response.statusText) // TODO Handle errors
@@ -15,7 +16,7 @@ export const checkBackend = async () => {
 }
 
 export const createPamelding = async (request: PameldingRequest): Promise<PameldingResponse> => {
-  return fetch('/amt-deltaker-bff/deltaker', {
+  return fetch(`${apiUrl()}amt-deltaker-bff/deltaker`, {
     method: 'POST',
     credentials: 'include',
     body: JSON.stringify(request)
