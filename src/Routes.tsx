f7627c0040f37e422fbe1ae0f8b3edea7e41815a
Magnus Evensberget <magnus.evensberget@nav.no>
2023-12-20 12:58:51 +0100

    Teste endring av base uten /

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,4 @@
-import {Navigate, Route, Routes} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 import {Page1} from './pages/Page1.tsx'
 import {Page2} from './pages/Page2.tsx'
 import App from './App.tsx'
@@ -13,10 +13,9 @@ export const AppRoutes = () => {
   return (
     <Routes>
       <Route path={`${base}`} element={<App/>}/>
-      <Route path={APP} element={<App/>}/>
-      <Route path={PAGE_1} element={<Page1/>}/>
-      <Route path={PAGE_2} element={<Page2/>}/>
-      <Route path={`${base}/*`} element={<Navigate replace to={`${base}`}/>}/>
+      <Route path={`${base}/${PAGE_1}`} element={<Page1/>}/>
+      <Route path={`${base}/${PAGE_2}`} element={<Page2/>}/>
+      {/*<Route path={`${base}/*`} element={<Navigate replace to={`${base}`}/>}/>*/}
     </Routes>
   )
 }
