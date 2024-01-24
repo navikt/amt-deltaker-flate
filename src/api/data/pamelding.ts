59eb509d1ee7be329c629085b522b3e2d078c325
Tormod Fossum <tormod.fossum@nav.no>
2024-01-24 17:12:11 +0100

    fiks formattering

--- src/api/data/pamelding.ts
+++ src/api/data/pamelding.ts
@@ -1,41 +1,41 @@
 import { z } from 'zod'
 
 export enum Tiltakstype {
-	ARBFORB = 'ARBFORB', // Arbeidsforberedende trening / AFT
-	ARBRRHDAG = 'ARBRRHDAG', // Arbeidsrettet rehabilitering / ARR
-	AVKLARAG = 'AVKLARAG', // Avklaring
-	INDOPPFAG = 'INDOPPFAG', // Oppfølging
-	DIGIOPPARB = 'DIGIOPPARB', // Digitalt oppfølgingstiltak
-	GRUFAGYRKE = 'GRUFAGYRKE', // Fag og yrkesopplæring gruppe / er kurs
-	GRUPPEAMO = 'GRUPPEAMO', // Arbeidsmarkedsopplæring i gruppe / er kurs
-	JOBBK = 'JOBBK', // Jobbklubb / er kurs
-	VASV = 'VASV' // Varig tilrettelagt arbeid (skjermet virksomhet) / VTA
+  ARBFORB = 'ARBFORB', // Arbeidsforberedende trening / AFT
+  ARBRRHDAG = 'ARBRRHDAG', // Arbeidsrettet rehabilitering / ARR
+  AVKLARAG = 'AVKLARAG', // Avklaring
+  INDOPPFAG = 'INDOPPFAG', // Oppfølging
+  DIGIOPPARB = 'DIGIOPPARB', // Digitalt oppfølgingstiltak
+  GRUFAGYRKE = 'GRUFAGYRKE', // Fag og yrkesopplæring gruppe / er kurs
+  GRUPPEAMO = 'GRUPPEAMO', // Arbeidsmarkedsopplæring i gruppe / er kurs
+  JOBBK = 'JOBBK', // Jobbklubb / er kurs
+  VASV = 'VASV' // Varig tilrettelagt arbeid (skjermet virksomhet) / VTA
 }
 
 export enum DeltakerStatusType {
-	KLADD = 'KLADD',
-	UTKAST_TIL_PAMELDING = 'UTKAST_TIL_PAMELDING',
-	VENTER_PA_OPPSTART = 'VENTER_PA_OPPSTART',
-	DELTAR = 'DELTAR',
-	HAR_SLUTTET = 'HAR_SLUTTET',
-	IKKE_AKTUELL = 'IKKE_AKTUELL',
-	FEILREGISTRERT = 'FEILREGISTRERT',
-	SOKT_INN = 'SOKT_INN',
-	VURDERES = 'VURDERES',
-	VENTELISTE = 'VENTELISTE',
-	AVBRUTT = 'AVBRUTT',
-	FULLFORT = 'FULLFORT',
-	PABEGYNT_REGISTRERING = 'PABEGYNT_REGISTRERING'
+  KLADD = 'KLADD',
+  UTKAST_TIL_PAMELDING = 'UTKAST_TIL_PAMELDING',
+  VENTER_PA_OPPSTART = 'VENTER_PA_OPPSTART',
+  DELTAR = 'DELTAR',
+  HAR_SLUTTET = 'HAR_SLUTTET',
+  IKKE_AKTUELL = 'IKKE_AKTUELL',
+  FEILREGISTRERT = 'FEILREGISTRERT',
+  SOKT_INN = 'SOKT_INN',
+  VURDERES = 'VURDERES',
+  VENTELISTE = 'VENTELISTE',
+  AVBRUTT = 'AVBRUTT',
+  FULLFORT = 'FULLFORT',
+  PABEGYNT_REGISTRERING = 'PABEGYNT_REGISTRERING'
 }
 
 export enum DeltakerStatusAarsakType {
-	SYK = 'SYK',
-	FATT_JOBB = 'FATT_JOBB',
-	TRENGER_ANNEN_STOTTE = 'TRENGER_ANNEN_STOTTE',
-	FIKK_IKKE_PLASS = 'FIKK_IKKE_PLASS',
-	IKKE_MOTT = 'IKKE_MOTT',
-	ANNET = 'ANNET',
-	AVLYST_KONTRAKT = 'AVLYST_KONTRAKT'
+  SYK = 'SYK',
+  FATT_JOBB = 'FATT_JOBB',
+  TRENGER_ANNEN_STOTTE = 'TRENGER_ANNEN_STOTTE',
+  FIKK_IKKE_PLASS = 'FIKK_IKKE_PLASS',
+  IKKE_MOTT = 'IKKE_MOTT',
+  ANNET = 'ANNET',
+  AVLYST_KONTRAKT = 'AVLYST_KONTRAKT'
 }
 
 export const tiltakstypeSchema = z.nativeEnum(Tiltakstype)
