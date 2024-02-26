e7333bd0424e42b506cb61b292706a2716e0f326
Line Moseng <line.moseng@nav.no>
2024-02-26 13:21:40 +0100

    Ny tekst på meld på direkte

--- src/components/opprett-pamelding/DelUtkastModal.tsx
+++ src/components/opprett-pamelding/DelUtkastModal.tsx
@@ -24,11 +24,13 @@ export const DelUtkastModal = ({
     <Modal open={open} header={{ heading: 'Del utkast og gjør klar vedtak' }} onClose={onCancel}>
       <Modal.Body>
         <BodyLong size="small">
-          Brukeren blir varslet, og finner lenke på Min side og i aktivitetsplanen. Bruker får lest gjennom hva du foreslår å sende til arrangøren. Bruker ser innhold, bakgrunnsinformasjon og navnet ditt.
+          Brukeren blir varslet, og finner lenke på Min side og i aktivitetsplanen. Bruker får lest
+          gjennom hva du foreslår å sende til arrangøren. Bruker ser innhold, bakgrunnsinformasjon og navnet ditt.
         </BodyLong>
 
         <BodyLong size="small" className="mt-6 mb-6">
-          Når brukeren godtar utkastet, så fattes vedtaket. I Deltakeroversikten på nav.no ser arrangøren påmeldingen, kontaktinformasjonen til bruker og tildelt veileder.
+          Når brukeren godtar utkastet, så fattes vedtaket. I Deltakeroversikten på nav.no ser arrangøren
+          påmeldingen, kontaktinformasjonen til bruker og tildelt veileder.
         </BodyLong>
 
         <BodyLong weight="semibold">
