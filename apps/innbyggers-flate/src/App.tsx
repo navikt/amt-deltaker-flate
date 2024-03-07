2234c25d453c9e573250364398db6b2ec9127b73
Marthe Frogner Holm <marthe@Marthe-sin-MacBook-Pro.local>
2024-03-07 12:46:37 +0100

    test med tailwind

--- apps/innbyggers-flate/src/App.tsx
+++ apps/innbyggers-flate/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
   return (
-    <Alert variant="info" className="m-4">
+    <Alert variant="info" className="m-4 xl:w-2">
       <Heading size="small">Hva har vi?</Heading>
       Dette er foreløpig alt
     </Alert>
