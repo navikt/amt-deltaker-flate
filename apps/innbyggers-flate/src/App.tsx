0f35f20e8a05780a4f35323b63f07356889973da
Marthe Frogner Holm <marthe@Marthe-sin-MacBook-Pro.local>
2024-03-08 10:59:32 +0100

    opprett pr workflow for innbyggers flate

--- apps/innbyggers-flate/src/App.tsx
+++ apps/innbyggers-flate/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
   return (
-    <Alert variant="info" className="m-4 xl:w-2">
+    <Alert variant="info" className="m-4">
       <Heading size="small">Hva har vi?</Heading>
       Dette er foreløpig alt
     </Alert>
