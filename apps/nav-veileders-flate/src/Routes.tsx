aea57a1465b78c5688bb14f0eafa56061a820726
Magnus Evensberget <magnus.evensberget@nav.no>
2024-03-19 15:35:12 +0100

    Redirect til Valps deltakeroversikt om siden ikke finnes

--- apps/nav-veileders-flate/src/Routes.tsx
+++ apps/nav-veileders-flate/src/Routes.tsx
@@ -1,27 +1,16 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
-import App from './App.tsx'
-import { SendTilbakePage } from './pages/SendTilbakePage.tsx'
 import { isEnvLocalDemoOrPr } from './utils/environment-utils.ts'
 import InngangSePaRediger from './InngangSePaRediger.tsx'
 import InngangMeldPa from './InngangMeldPa.tsx'
-
-const appUrl = (path: string): string => {
-  const strippedPath = path.startsWith('/') ? path.substring(1) : path
-  return `${import.meta.env.BASE_URL.replace(/^\//, '')}${strippedPath}`
-}
-
-export const base = appUrl('arbeidsmarkedstiltak/tiltak/:id/deltaker')
-export const TILBAKE_PAGE = 'tilbake'
+import RedirectToDeltakeroversikt from './guards/RedirectToDeltakeroversikt.tsx'
 
 export const AppRoutes = () => {
 
   return (
     <Routes>
-      <Route path={'/'} element={<App />} />
       <Route path={'/arbeidsmarkedstiltak/deltakelse/deltaker/:deltakerId'} element={<InngangSePaRediger />} />
       <Route path={'/arbeidsmarkedstiltak/deltakelse/:deltakerlisteId'} element={<InngangMeldPa/>}/>
-      <Route path={`/${TILBAKE_PAGE}`} element={<SendTilbakePage />} />
-      <Route path={'/*'} element={<Navigate replace to={'/'} />} />
+      <Route path={'/*'} element={<RedirectToDeltakeroversikt/>} />
       {isEnvLocalDemoOrPr && (
         <>
           <Route path={'*'} element={<Navigate replace to={'/'} />} />
