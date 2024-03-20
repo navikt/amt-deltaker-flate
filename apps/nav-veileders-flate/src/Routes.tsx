9cdbd03d1d6d4931031e014fed3258be93b36a04
Magnus Evensberget <magnus.evensberget@nav.no>
2024-03-20 10:34:23 +0100

    Fikse lokalt

--- apps/nav-veileders-flate/src/Routes.tsx
+++ apps/nav-veileders-flate/src/Routes.tsx
@@ -8,13 +8,15 @@ export const AppRoutes = () => {
 
   return (
     <Routes>
-      <Route path={'/arbeidsmarkedstiltak/deltakelse/deltaker/:deltakerId'} element={<InngangSePaRediger />} />
+      <Route path={'/arbeidsmarkedstiltak/deltakelse/deltaker/:deltakerId'} element={<InngangSePaRediger/>}/>
       <Route path={'/arbeidsmarkedstiltak/deltakelse/:deltakerlisteId'} element={<InngangMeldPa/>}/>
-      <Route path={'/*'} element={<RedirectToDeltakeroversikt/>} />
+      {!isEnvLocalDemoOrPr && (
+        <Route path={'/*'} element={<RedirectToDeltakeroversikt/>}/>
+      )}
       {isEnvLocalDemoOrPr && (
-        <>
-          <Route path={'*'} element={<Navigate replace to={'/'} />} />
-        </>
+        <Route path={'/*'} element={
+          <Navigate replace to={'/arbeidsmarkedstiltak/deltakelse/15462eb2-9fb0-4e37-b749-fe71a9af8d48'}/>}
+        />
       )}
     </Routes>
   )
