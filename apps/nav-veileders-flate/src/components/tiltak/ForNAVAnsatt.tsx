e9d7eae003afa6751968c83ae1baf1f14d81e8b9
Marthe Frogner Holm <marthe@Marthe-sin-MBP.localdomain>
2024-04-03 10:33:00 +0200

    fiks dobbel a for linkPanel

--- apps/nav-veileders-flate/src/components/tiltak/ForNAVAnsatt.tsx
+++ apps/nav-veileders-flate/src/components/tiltak/ForNAVAnsatt.tsx
@@ -3,7 +3,7 @@ import { EndreDeltakelseKnapp } from './EndreDeltakelseKnapp.tsx'
 import { hentTiltakNavnHosArrangorTekst } from '../../utils/displayText.ts'
 import { usePameldingCOntext } from './PameldingContext.tsx'
 import { formatDateFromString } from '../../utils/utils.ts'
-import { TiltaksgjennomforingLink } from '../TiltaksgjennomforingLink.tsx'
+import { TILTAKSGJENNOMFORING_LINK, useModiaLink } from '../../hooks/useModiaLink.ts'
 
 interface Props {
   className: string
@@ -11,6 +11,8 @@ interface Props {
 
 export const ForNAVAnsatt = ({ className }: Props) => {
   const { pamelding } = usePameldingCOntext()
+  const deltakerlisteId = pamelding.deltakerliste.deltakerlisteId
+  const { doRedirect } = useModiaLink()
 
   return (
     <div className={`bg-white p-4 h-fit ${className} flex flex-col`}>
@@ -19,25 +21,31 @@ export const ForNAVAnsatt = ({ className }: Props) => {
       </Heading>
       <EndreDeltakelseKnapp />
 
-      <TiltaksgjennomforingLink deltakerlisteId={pamelding.deltakerliste.deltakerlisteId}>
-        <LinkPanel border className="mt-4 rounded border-2 border-[var(--a-border-selected)]">
-          <LinkPanel.Title className="text-lg text-[var(--a-text-action)] text-nowrap">
-            Gå til tiltaksgjennomføringen
-          </LinkPanel.Title>
-          <LinkPanel.Description>
-            <BodyShort size="small">
-              {hentTiltakNavnHosArrangorTekst(
-                pamelding.deltakerliste.tiltakstype,
-                pamelding.deltakerliste.arrangorNavn
-              )}
-            </BodyShort>
-            <BodyShort size="small">
-              {formatDateFromString(pamelding.deltakerliste.startdato)} -{' '}
-              {formatDateFromString(pamelding.deltakerliste.sluttdato)}
-            </BodyShort>
-          </LinkPanel.Description>
-        </LinkPanel>
-      </TiltaksgjennomforingLink>
+      <LinkPanel
+        href={`${TILTAKSGJENNOMFORING_LINK}/${deltakerlisteId}`}
+        onClick={(event) => {
+          event.preventDefault()
+          doRedirect(`${TILTAKSGJENNOMFORING_LINK}/${deltakerlisteId}`)
+        }}
+        border
+        className="mt-4 rounded border-2 border-[var(--a-border-selected)]"
+      >
+        <LinkPanel.Title className="text-lg text-[var(--a-text-action)] text-nowrap">
+          Gå til tiltaksgjennomføringen
+        </LinkPanel.Title>
+        <LinkPanel.Description>
+          <BodyShort size="small">
+            {hentTiltakNavnHosArrangorTekst(
+              pamelding.deltakerliste.tiltakstype,
+              pamelding.deltakerliste.arrangorNavn
+            )}
+          </BodyShort>
+          <BodyShort size="small">
+            {formatDateFromString(pamelding.deltakerliste.startdato)} -{' '}
+            {formatDateFromString(pamelding.deltakerliste.sluttdato)}
+          </BodyShort>
+        </LinkPanel.Description>
+      </LinkPanel>
     </div>
   )
 }
