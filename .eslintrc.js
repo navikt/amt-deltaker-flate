6d9a720c1748f5c0767cac334a2773584f75eba5
Andreas Nordahl <andnorda@gmail.com>
2024-04-05 11:48:26 +0200

    fix eslint config?

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 // This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ['apps/**', 'shared-config/**'],
+  ignorePatterns: ['shared-config/**'],
   extends: ['@amt-deltaker-flate/eslint-config/library.js'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
