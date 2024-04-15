92d50e90cefea33d6064dbfd697c9d9d3eb37324
MartheFH <136322563+MartheFH@users.noreply.github.com>
2024-04-15 09:04:40 +0200

    Dekoratoren for innbyggers flate (#259)

--- apps/innbyggers-flate/src/main.tsx
+++ apps/innbyggers-flate/src/main.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { worker } from './mocks/setupMocks.ts'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import { useMock } from './utils/environment-utils.ts'
+import { isDev, isProd, useMock } from './utils/environment-utils.ts'
 import { AppRoutes } from './Routes.tsx'
+import { injectDecoratorClientSide } from '@navikt/nav-dekoratoren-moduler'
 
 export async function enableMocking() {
   if (useMock) {
@@ -20,7 +21,21 @@ export async function enableMocking() {
   }
 }
 
-const renderApp = () => {
+// list of parameters and default values: https://github.com/navikt/nav-dekoratoren?tab=readme-ov-file#parametere
+const setupNavDekorator = () => {
+  return injectDecoratorClientSide({
+    env: isProd() ? 'prod' : 'dev',
+    params: {
+      level: 'Level4',
+      enforceLogin: true,
+      logoutWarning: isProd() || isDev()
+    }
+  })
+}
+
+const renderApp = async () => {
+  await setupNavDekorator()
+
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <BrowserRouter>
