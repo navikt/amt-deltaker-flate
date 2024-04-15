37f28b2aff99f2e6e91219789c518c10f3a7bbc1
MartheFH <136322563+MartheFH@users.noreply.github.com>
2024-04-15 14:01:24 +0200

    Sette opp grafana faro for innbyggers flate (#265)

--- apps/innbyggers-flate/src/main.tsx
+++ apps/innbyggers-flate/src/main.tsx
@@ -6,6 +6,7 @@ import './index.css'
 import { isDev, isProd, useMock } from './utils/environment-utils.ts'
 import { AppRoutes } from './Routes.tsx'
 import { injectDecoratorClientSide } from '@navikt/nav-dekoratoren-moduler'
+import { initializeFaro } from '@grafana/faro-web-sdk'
 
 export async function enableMocking() {
   if (useMock) {
@@ -43,6 +44,15 @@ const renderApp = async () => {
       </BrowserRouter>
     </React.StrictMode>
   )
+}
+
+if (import.meta.env.VITE_FARO_URL) {
+  initializeFaro({
+    url: import.meta.env.VITE_FARO_URL,
+    app: {
+      name: 'amt-deltaker-innbyggers-flate'
+    }
+  })
 }
 
 if (useMock) {
