dfc5ef2016b1c637a3a1254cc064ebaf97514776
Tia Firing <tia.firing@nav.no>
2024-04-15 13:46:20 +0200

    innbyggers tiltaksside

---
+++ apps/innbyggers-flate/src/components/HvaErDette.tsx
@@ -0,0 +1,37 @@
+import { BodyLong, Detail, Heading } from '@navikt/ds-react'
+import { formatDateStrWithMonthName } from '../utils/utils.ts'
+import { Vedtaksinformasjon } from '../api/data/deltaker.ts'
+
+interface Props {
+  vedtaksinformasjon: Vedtaksinformasjon | null
+  className: string
+}
+
+export const HvaErDette = ({ vedtaksinformasjon, className }: Props) => {
+  const harNavMeldtPaDirekte = vedtaksinformasjon?.fattetAvNav
+
+  return (
+    <div className={className}>
+      <Heading level="2" size="medium">
+        Hva er dette?
+      </Heading>
+      <BodyLong size="small">
+        Dette er et vedtak etter arbeidsmarkedsloven § 12 og forskrift om
+        arbeidsmarkedstiltak kapittel 4.
+      </BodyLong>
+      {vedtaksinformasjon && (
+        <Detail className="mt-2">
+          {harNavMeldtPaDirekte
+            ? `Meldt på: ${formatDateStrWithMonthName(vedtaksinformasjon.fattet)} av ${vedtakEndretAv(vedtaksinformasjon)}.`
+            : `Utkast delt av: ${vedtakEndretAv(vedtaksinformasjon)}. Du godkjente ${formatDateStrWithMonthName(vedtaksinformasjon.fattet)}.`}
+        </Detail>
+      )}
+    </div>
+  )
+}
+
+const vedtakEndretAv = (vedtaksinformasjon: Vedtaksinformasjon): string => {
+  if (vedtaksinformasjon.sistEndretAvEnhet === null)
+    return vedtaksinformasjon.sistEndretAv
+  return `${vedtaksinformasjon.sistEndretAv} ${vedtaksinformasjon.sistEndretAvEnhet}`
+}
--- apps/innbyggers-flate/src/pages/TiltakPage.tsx
+++ apps/innbyggers-flate/src/pages/TiltakPage.tsx
@@ -1,3 +1,219 @@
+import { useDeltakerContext } from '../DeltakerContext.tsx'
+import {
+  BodyLong,
+  BodyShort,
+  ExpansionCard,
+  Heading,
+  HStack,
+  Label,
+  Link,
+  LinkPanel,
+  List
+} from '@navikt/ds-react'
+import {
+  EMDASH,
+  formatDateFromString,
+  getDeltakerStatusAarsakText,
+  hentTiltakNavnHosArrangørTekst,
+  INNHOLD_TYPE_ANNET
+} from '../utils/utils.ts'
+import {
+  DeltakerResponse,
+  DeltakerStatusType,
+  Tiltakstype
+} from '../api/data/deltaker.ts'
+import { ChatElipsisIcon, ChevronRightIcon } from '@navikt/aksel-icons'
+import { getDialogUrl } from '../utils/environment-utils.ts'
+import { HvaErDette } from '../components/HvaErDette.tsx'
+import { DeltakerIStatusTag } from '../components/DeltakerIStatusTag.tsx'
+
+const skalViseDeltakelsesmengde = (deltaker: DeltakerResponse) => {
+  return (
+    deltaker.deltakerliste.tiltakstype == Tiltakstype.ARBFORB ||
+    deltaker.deltakerliste.tiltakstype == Tiltakstype.VASV
+  )
+}
+
+const deltakelsesmengdeString = (deltaker: DeltakerResponse): string => {
+  if (deltaker.dagerPerUke !== null) {
+    if (deltaker.dagerPerUke === 1) {
+      return `${deltaker.deltakelsesprosent ?? 100}\u00A0% ${deltaker.dagerPerUke} dag i uka`
+    } else {
+      return `${deltaker.deltakelsesprosent ?? 100}\u00A0% ${deltaker.dagerPerUke} dager i uka`
+    }
+  }
+
+  return `${deltaker.deltakelsesprosent}\u00A0%`
+}
+
 export const TiltakPage = () => {
-  return <div className="flex flex-col items-start">Tiltaksiden</div>
+  const { deltaker } = useDeltakerContext()
+
+  const tiltakOgStedTekst = hentTiltakNavnHosArrangørTekst(
+    deltaker.deltakerliste.tiltakstype,
+    deltaker.deltakerliste.arrangorNavn
+  )
+  const skalViseDato =
+    deltaker.status.type !== DeltakerStatusType.IKKE_AKTUELL &&
+    deltaker.status.type !== DeltakerStatusType.AVBRUTT_UTKAST
+  const bakgrunsinformasjon =
+    deltaker.bakgrunnsinformasjon && deltaker.bakgrunnsinformasjon.length > 0
+      ? deltaker.bakgrunnsinformasjon
+      : EMDASH
+
+  let dato = EMDASH
+  if (
+    deltaker.startdato &&
+    deltaker.sluttdato &&
+    deltaker.startdato !== EMDASH &&
+    deltaker.sluttdato !== EMDASH
+  ) {
+    dato = `${formatDateFromString(deltaker.startdato)} - ${formatDateFromString(deltaker.sluttdato)}`
+  } else if (deltaker.startdato && deltaker.startdato !== EMDASH) {
+    dato = formatDateFromString(deltaker.startdato)
+  }
+
+  return (
+    <div className={'bg-white px-12 py-4 w-full xl:w-[65%]'}>
+      <Heading level="1" size="large">
+        {tiltakOgStedTekst}
+      </Heading>
+
+      <HStack gap="2" className="mt-8">
+        <Label>Status:</Label>
+        <DeltakerIStatusTag statusType={deltaker.status.type} />
+      </HStack>
+      {deltaker.status.aarsak && (
+        <HStack gap="2" className="mt-4">
+          <Label>Årsak:</Label>
+          <BodyShort>
+            {getDeltakerStatusAarsakText(deltaker.status.aarsak)}
+          </BodyShort>
+        </HStack>
+      )}
+      {skalViseDato && (
+        <HStack gap="2" className="mt-4">
+          <Label>
+            {deltaker.startdato && !deltaker.sluttdato
+              ? 'Oppstartsdato:'
+              : 'Dato:'}
+          </Label>
+          <BodyShort>{dato}</BodyShort>
+        </HStack>
+      )}
+      <BodyLong size="small" className="mt-4">
+        {`Du er meldt på arbeidsmarkedstiltaket: ${tiltakOgStedTekst}. Når arrangøren har en ledig plass så vil de ta kontakt med deg for å avklare oppstart.`}
+      </BodyLong>
+
+      <Heading level="2" size="medium" className="mt-8">
+        Hva er innholdet?
+      </Heading>
+      <BodyLong className="mt-4" size="small">
+        {deltaker.deltakelsesinnhold?.ledetekst ?? ''}
+      </BodyLong>
+      {deltaker.deltakelsesinnhold && (
+        <List as="ul" size="small" className="mt-4">
+          {deltaker.deltakelsesinnhold.innhold
+            .filter((i) => i.valgt)
+            .map((i) => (
+              <List.Item key={i.innholdskode}>
+                {i.innholdskode === INNHOLD_TYPE_ANNET
+                  ? i.beskrivelse
+                  : i.tekst}
+              </List.Item>
+            ))}
+        </List>
+      )}
+      <div>
+        <Heading level="2" size="medium" className="mt-8">
+          Bakgrunnsinfo
+        </Heading>
+        <BodyLong size="small" className="mt-2">
+          {bakgrunsinformasjon}
+        </BodyLong>
+
+        {skalViseDeltakelsesmengde(deltaker) && (
+          <>
+            <Heading level="2" size="medium" className="mt-8">
+              Deltakelsesmengde
+            </Heading>
+            <BodyLong size="small" className="mt-2">
+              {deltakelsesmengdeString(deltaker)}
+            </BodyLong>
+          </>
+        )}
+
+        <Link href="#" className="mt-8">
+          {/* TODO: lenke til riktig sted */}
+          Se endringer
+          <span>
+            <ChevronRightIcon
+              title="Gå til side for endringer"
+              className="text-2xl"
+            />
+          </span>
+        </Link>
+
+        <LinkPanel href={getDialogUrl()} className="mt-8 rounded-lg">
+          <div className="grid grid-flow-col items-center gap-4">
+            <ChatElipsisIcon className="text-2xl" />
+            <span>
+              Send en melding her til NAV-veilederen din hvis noe skal endres.
+            </span>
+          </div>
+        </LinkPanel>
+
+        <HvaErDette
+          vedtaksinformasjon={deltaker.vedtaksinformasjon}
+          className="mt-8"
+        />
+
+        <Heading level="2" size="medium" className="mt-8">
+          Du har rett til å klage
+        </Heading>
+        <BodyLong size="small" className="mt-1">
+          Du kan klage hvis du ikke ønsker å delta, er uenig i endringer på
+          deltakelsen eller du ønsker et annet arbeidsmarkedstiltak. Fristen for
+          å klage er seks uker etter du mottok informasjonen. Les mer om{' '}
+          {
+            <Link href="#">
+              {/* TODO: lenke til klageinfoside */}
+              retten til å klage her.
+            </Link>
+          }
+        </BodyLong>
+
+        <ExpansionCard aria-label="Demo med bare tittel" className="mt-8">
+          <ExpansionCard.Header>
+            <ExpansionCard.Title>Hva deles med arrangøren?</ExpansionCard.Title>
+          </ExpansionCard.Header>
+          <ExpansionCard.Content>
+            <BodyLong size="small">
+              NAV samarbeider med {deltaker.deltakerliste.arrangorNavn}.
+              Arrangøren behandler opplysninger på vegne av NAV.
+            </BodyLong>
+            <List as="ul" size="small">
+              <List.Item>
+                Navn og kontaktinformasjonen til NAV-veilederen din
+              </List.Item>
+              <List.Item>
+                Påmeldingen: Innholdet og bakgrunnsinformasjon
+              </List.Item>
+              <List.Item>Navn og fødselsnummer</List.Item>
+              <List.Item>Telefonnummer og e-postadresse</List.Item>
+              {deltaker.adresseDelesMedArrangor && (
+                <List.Item>Adresse</List.Item>
+              )}
+            </List>
+            <Link
+              href="http://nav.no/person/personopplysninger/"
+              className="text-base"
+            >
+              Se her hvilke opplysninger NAV har om deg.
+            </Link>
+          </ExpansionCard.Content>
+        </ExpansionCard>
+      </div>
+    </div>
+  )
 }
