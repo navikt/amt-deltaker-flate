dfc9bc3350e94b838bf038471fcd4be6ba98a6af
Tormod Fossum <tormod.fossum@nav.no>
2024-04-15 13:35:37 +0200

    Sett forskriftskapittel basert på hvilket tiltak det gjelder

--- apps/nav-veileders-flate/src/components/tiltak/HvaErDette.tsx
+++ apps/nav-veileders-flate/src/components/tiltak/HvaErDette.tsx
@@ -1,13 +1,18 @@
 import { BodyLong, Detail, Heading } from '@navikt/ds-react'
 import { formatDateStrWithMonthName } from '../../utils/utils.ts'
-import { Vedtaksinformasjon } from '../../api/data/pamelding.ts'
+import { Tiltakstype, Vedtaksinformasjon } from '../../api/data/pamelding.ts'
 
 interface Props {
+  tiltakstype: Tiltakstype
   vedtaksinformasjon: Vedtaksinformasjon | null
   className: string
 }
 
-export const HvaErDette = ({ vedtaksinformasjon, className }: Props) => {
+export const HvaErDette = ({
+  tiltakstype,
+  vedtaksinformasjon,
+  className
+}: Props) => {
   const harNavMeldtPaDirekte = vedtaksinformasjon?.fattetAvNav
 
   return (
@@ -16,8 +21,8 @@ export const HvaErDette = ({ vedtaksinformasjon, className }: Props) => {
         Hva er dette?
       </Heading>
       <BodyLong size="small">
-        Dette er et vedtak etter arbeidsmarkedsloven § 12 og forskrift om
-        arbeidsmarkedstiltak kapittel 4.
+        {`Dette er et vedtak etter arbeidsmarkedsloven § 12 og forskrift om
+				arbeidsmarkedstiltak kapittel ${forskriftskapitler[tiltakstype]}.`}
       </BodyLong>
       {vedtaksinformasjon && (
         <Detail className="mt-2">
@@ -34,4 +39,16 @@ const vedtakEndretAv = (vedtaksinformasjon: Vedtaksinformasjon): string => {
   if (vedtaksinformasjon.sistEndretAvEnhet === null)
     return vedtaksinformasjon.sistEndretAv
   return `${vedtaksinformasjon.sistEndretAv} ${vedtaksinformasjon.sistEndretAvEnhet}`
+}
+
+const forskriftskapitler: { [Key in Tiltakstype]: string } = {
+  [Tiltakstype.ARBFORB]: '13',
+  [Tiltakstype.ARBRRHDAG]: '12',
+  [Tiltakstype.AVKLARAG]: '2',
+  [Tiltakstype.INDOPPFAG]: '4',
+  [Tiltakstype.DIGIOPPARB]: '4',
+  [Tiltakstype.GRUFAGYRKE]: '7',
+  [Tiltakstype.GRUPPEAMO]: '7',
+  [Tiltakstype.JOBBK]: '4',
+  [Tiltakstype.VASV]: '14'
 }
