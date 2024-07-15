489c242c82305aee2aedb7850563d9a5c2c7c4be
Tormod Fossum <tormod.fossum@nav.no>
2024-07-15 12:52:21 +0200

    Trekk ut BegrunnelseInput

--- apps/nav-veileders-flate/src/components/tiltak/endre-deltakelse-modaler/IkkeAktuellModal.tsx
+++ apps/nav-veileders-flate/src/components/tiltak/endre-deltakelse-modaler/IkkeAktuellModal.tsx
@@ -16,8 +16,8 @@ import {
 import { PameldingResponse } from '../../../api/data/pamelding.ts'
 import { getDeltakerStatusAarsakTypeText } from '../../../utils/displayText.ts'
 import { getDeltakerStatusAarsakTyperAsList } from '../../../utils/utils.ts'
-import { BEGRUNNELSE_MAKS_TEGN } from '../../../model/PameldingFormValues.ts'
-import { Endringsmodal } from '../../modal/Endringsmodal.tsx'
+import { Endringsmodal } from '../modal/Endringsmodal.tsx'
+import { BegrunnelseInput, useBegrunnelse } from '../modal/BegrunnelseInput.tsx'
 
 interface IkkeAktuellModalProps {
   pamelding: PameldingResponse
@@ -53,15 +53,14 @@ export const IkkeAktuellModal = ({
   const [errorAarsak, setErrorAarsak] = useState<boolean>(false)
   const [errorAarsakAnnet, setErrorAarsakAnnet] = useState<boolean>(false)
 
-  const [begrunnelse, setBegrunnelse] = useState<string | null>()
-  const [errorBegrunnelse, setErrorBegrunnelse] = useState<string | null>(null)
+  const begrunnelse = useBegrunnelse(true)
 
   const aarsakErAnnet = valgtArsak === DeltakerStatusAarsakType.ANNET
+
   const harAnnetBeskrivelse = beskrivelse && beskrivelse.length > 0
   const harForLangAnnetBeskrivelse =
     harAnnetBeskrivelse && beskrivelse.length > BESKRIVELSE_ARSAK_ANNET_MAX_TEGN
-  const harForLangBegrunnelse =
-    begrunnelse && begrunnelse.length > BEGRUNNELSE_MAKS_TEGN
+
   const { enhetId } = useAppContext()
 
   const validertRequest = () => {
@@ -71,24 +70,22 @@ export const IkkeAktuellModal = ({
       hasError = true
     }
 
+    if (!begrunnelse.valider()) {
+      hasError = true
+    }
+
     if (aarsakErAnnet && (!harAnnetBeskrivelse || harForLangAnnetBeskrivelse)) {
       setErrorAarsakAnnet(true)
       hasError = true
     }
 
-    if (harForLangBegrunnelse) {
-      setErrorBegrunnelse(
-        `Begrunnelsen kan ikke være mer enn ${BEGRUNNELSE_MAKS_TEGN} tegn`
-      )
-      hasError = true
-    }
     if (!hasError && valgtArsak !== null) {
       const endring: IkkeAktuellRequest = {
         aarsak: {
           type: valgtArsak!,
           beskrivelse: aarsakErAnnet ? beskrivelse : null
         },
-        begrunnelse: begrunnelse || null,
+        begrunnelse: begrunnelse.begrunnelse || null,
         forslagId: forslag ? forslag.id : null
       }
 
@@ -110,6 +107,7 @@ export const IkkeAktuellModal = ({
       onSend={onSuccess}
       apiFunction={endreDeltakelseIkkeAktuell}
       validertRequest={validertRequest}
+      begrunnelse="optional"
       forslag={forslag}
     >
       <RadioGroup
@@ -154,20 +152,10 @@ export const IkkeAktuellModal = ({
           )}
         </>
       </RadioGroup>
-      <Textarea
-        onChange={(e) => {
-          setBegrunnelse(e.target.value)
-          setErrorBegrunnelse(null)
-        }}
-        error={errorBegrunnelse}
-        className="mt-6"
-        label="Begrunnelse for at deltakeren ikke er aktuell (valgfri)"
-        description="Beskriv kort hvorfor endringen er riktig for personen."
-        value={begrunnelse ?? ''}
-        maxLength={BEGRUNNELSE_MAKS_TEGN}
-        id="begrunnelse"
-        size="small"
-        aria-label={'Begrunnelse'}
+      <BegrunnelseInput
+        valgfri
+        onChange={begrunnelse.handleChange}
+        error={begrunnelse.error}
       />
     </Endringsmodal>
   )
