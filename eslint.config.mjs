278375d31c97db1d9e94213be61665fcba56b6bc
Marthe Frogner Holm <marthe@Marthe-sin-MBP.localdomain>
2024-07-30 11:45:58 +0200

    Migrate eslint to v9

---
+++ eslint.config.mjs
@@ -0,0 +1,5 @@
+// This configuration only applies to the package manager root.
+import base from '@amt-deltaker-flate/eslint-config/eslint.config.js'
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [...base]
