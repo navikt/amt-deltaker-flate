aa8cd4772a913134a85670765fb65994ed76841b
Line Moseng <line.moseng@nav.no>
2024-08-07 12:00:40 +0200

    Legger til lenke til navet

--- apps/nav-veileders-flate/src/components/tiltak/modal/Avvisningsmodal.tsx
+++ apps/nav-veileders-flate/src/components/tiltak/modal/Avvisningsmodal.tsx
@@ -5,7 +5,7 @@ import {
 } from 'deltaker-flate-common'
 import { PameldingResponse } from '../../../api/data/pamelding'
 import { ReactNode } from 'react'
-import { Alert, Detail, Modal } from '@navikt/ds-react'
+import { Alert, Detail, Link, Modal } from '@navikt/ds-react'
 import { ErrorPage } from '../../../pages/ErrorPage'
 import { ModalForslagDetaljer } from '../forslag/ModalForslagDetaljer'
 import { avvisForslag } from '../../../api/api'
@@ -18,6 +18,9 @@ interface Props {
   forslag: AktivtForslag
   children?: ReactNode
 }
+
+export const RUTINE_NAVET_LINK =
+  'https://navno.sharepoint.com/sites/fag-og-ytelser-arbeid-tiltak-og-virkemidler/SitePages/Klage-p%C3%A5-arbeidsmarkedstiltak.aspx'
 
 export default function AvvisningsmodalBody({
   onSend,
@@ -53,8 +56,10 @@ export default function AvvisningsmodalBody({
         {children}
         <Alert variant="info" className="mt-4">
           Vurder om dette er et ønske fra brukeren og om du skal sende et
-          avslagsbrev. Les mer på Navet om rutinen for avslag og klage på
-          arbeidsmarkedstiltak.
+          avslagsbrev. Les mer på Navet om{' '}
+          <Link href={RUTINE_NAVET_LINK}>
+            rutinen for avslag og klage på arbeidsmarkedstiltak.
+          </Link>
         </Alert>
       </Modal.Body>
       <ModalFooter
