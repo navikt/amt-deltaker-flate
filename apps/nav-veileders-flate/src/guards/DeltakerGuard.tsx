1bc0c288b88511e8b65d27e61df902c20388288b
Marthe Frogner Holm <marthe@Marthe-sin-MBP.localdomain>
2024-08-20 14:15:11 +0200

    Se om vi får med personident endringer

--- apps/nav-veileders-flate/src/guards/DeltakerGuard.tsx
+++ apps/nav-veileders-flate/src/guards/DeltakerGuard.tsx
@@ -1,13 +1,20 @@
 import dayjs from 'dayjs'
 import nb from 'dayjs/locale/nb'
+import duration from 'dayjs/plugin/duration'
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
-import duration from 'dayjs/plugin/duration'
 import { DeltakerStatusType } from 'deltaker-flate-common'
+import { useEffect, useState } from 'react'
+import { useAppContext } from '../AppContext.tsx'
 import { usePameldingContext } from '../components/tiltak/PameldingContext.tsx'
+import {
+  DELTAKELSESOVERSIKT_LINK,
+  useModiaLink
+} from '../hooks/useModiaLink.ts'
 import { OpprettPameldingPage } from '../pages/OpprettPameldingPage.tsx'
 import { RedigerPameldingPage } from '../pages/RedigerPameldingPage.tsx'
 import { TiltakPage } from '../pages/TiltakPage.tsx'
+import { isEnvLocalDemoOrPr } from '../utils/environment-utils.ts'
 
 dayjs.locale(nb)
 dayjs.extend(isSameOrAfter)
@@ -16,6 +23,17 @@ dayjs.extend(duration)
 
 export const DeltakerGuard = () => {
   const { pamelding } = usePameldingContext()
+  const { doRedirect } = useModiaLink()
+  const { personident } = useAppContext()
+  const [isFirstRender, setIsFirstRender] = useState(true)
+
+  useEffect(() => {
+    if (isFirstRender) {
+      setIsFirstRender(false)
+    } else if (!isEnvLocalDemoOrPr) {
+      doRedirect(DELTAKELSESOVERSIKT_LINK)
+    }
+  }, [personident])
 
   let pageToLoad = null
 
