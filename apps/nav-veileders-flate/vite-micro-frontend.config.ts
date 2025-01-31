342a6599c9d21e94eab333d52e543f72f5244bd0
Marthe Frogner Holm <marthe@Mac.localdomain>
2025-01-31 13:34:47 +0100

    fiks vite config

--- apps/nav-veileders-flate/vite-micro-frontend.config.ts
+++ apps/nav-veileders-flate/vite-micro-frontend.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
       input: 'src/webComponentWrapper.tsx'
