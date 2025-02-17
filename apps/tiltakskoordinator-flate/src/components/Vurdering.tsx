f8c54c25758b8f3feafcc128c719c500189ad8f1
Izis Frøyhov <izis.froyhov@nav.no>
2025-02-17 16:15:47 +0100

    Legger til ikon på vurdering

--- apps/tiltakskoordinator-flate/src/components/Vurdering.tsx
+++ apps/tiltakskoordinator-flate/src/components/Vurdering.tsx
@@ -1,12 +1,36 @@
 import { Vurderingstype } from '../api/data/deltakerliste.ts'
 import { BodyShort } from '@navikt/ds-react'
+import { XMarkOctagonIcon } from '@navikt/aksel-icons'
 
 interface Props {
   vurdering: Vurderingstype | null
 }
 
 export const Vurdering = ({ vurdering }: Props) => {
-  return <BodyShort>{vurderingDisplayTekst(vurdering)}</BodyShort>
+  if (!vurdering) return null
+  return (
+    <>
+      <BodyShort size="small">
+        <XMarkOctagonIcon
+          color={ikonFarge(vurdering)}
+          fontSize="1.5rem"
+          className="inline pb-0.5"
+        />
+        <>{vurderingDisplayTekst(vurdering)}</>
+      </BodyShort>
+    </>
+  )
+}
+
+function ikonFarge(vurdering: Vurderingstype | null) {
+  switch (vurdering) {
+    case Vurderingstype.OPPFYLLER_KRAVENE:
+      return 'var(--a-nav-red)'
+    case Vurderingstype.OPPFYLLER_IKKE_KRAVENE:
+      return 'var(--a-surface-success)'
+    default:
+      return ''
+  }
 }
 
 function vurderingDisplayTekst(vurdering: Vurderingstype | null) {
