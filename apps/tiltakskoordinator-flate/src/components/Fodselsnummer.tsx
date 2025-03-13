d90d363b3d50c90d2b25712892b4c5af614dc267
Marthe Frogner Holm <marthe@Mac.localdomain>
2025-03-13 20:54:45 +0100

    vis koiert success ikon

--- apps/tiltakskoordinator-flate/src/components/Fodselsnummer.tsx
+++ apps/tiltakskoordinator-flate/src/components/Fodselsnummer.tsx
@@ -1,20 +1,31 @@
-import { FilesIcon } from '@navikt/aksel-icons'
+import { CheckmarkIcon, FilesIcon } from '@navikt/aksel-icons'
 import { BodyShort, Button, Tooltip } from '@navikt/ds-react'
+import { useState } from 'react'
 
 interface FnrProps {
   fnr: string
 }
 
 export const Fodselsnummer = ({ fnr }: FnrProps): React.ReactElement => {
+  const [copyDone, setCopyDone] = useState(false)
+
   return (
-    <Tooltip content="Kopier fødselsnummer">
+    <Tooltip content={copyDone ? 'Kopiert' : 'Kopier fødselsnummer'}>
       <Button
-        icon={<FilesIcon aria-hidden />}
+        icon={
+          copyDone ? <CheckmarkIcon aria-hidden /> : <FilesIcon aria-hidden />
+        }
         variant="tertiary-neutral"
         size="small"
         iconPosition="right"
         className="p-0"
-        onClick={() => navigator.clipboard.writeText(fnr)}
+        onClick={() => {
+          navigator.clipboard.writeText(fnr)
+          setCopyDone(true)
+          setTimeout(() => {
+            setCopyDone(false)
+          }, 2000)
+        }}
       >
         <BodyShort size="small" weight="semibold">
           F.nr: {fnr}
