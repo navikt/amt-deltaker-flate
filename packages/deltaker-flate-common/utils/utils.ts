286bcf53f10dabe81acf505a59baab45ddb26e43
MartheFH <136322563+MartheFH@users.noreply.github.com>
2025-06-02 12:47:41 +0200

    Fjern flere spesialtegn (#859)

--- packages/deltaker-flate-common/utils/utils.ts
+++ packages/deltaker-flate-common/utils/utils.ts
@@ -121,12 +121,22 @@ export const haveSameContents = (list1: unknown[], list2: unknown[]) =>
 export const fjernUgyldigeTegn = (str: string) => {
   const ugyldigeTegnRegex =
     /[^a-zA-ZæøåÆØÅ0-9 \s.,@<>'`!"#$%&\/()\\=?*<;:>*§_-]/g
-  const stringUtenNonBreakingSace = str.replace(/\u00A0/g, ' ')
-  const stringUtenZeroWidthNonBreakingSace = stringUtenNonBreakingSace.replace(
-    /\uFEFF/g,
-    ''
+
+  const cleanText = str.replace(
+    new RegExp(
+      // NBSP, Narrow NBSP, ZWNBSP, Word Joiner, Zero-width space, ZWNJ, ZWJ
+      '[\\u00A0\\u202F\\uFEFF\\u2060\\u200B\\u200C\\u200D]|' +
+        ugyldigeTegnRegex.source,
+      'g'
+    ),
+    (match) => {
+      // NBSP (NonBreakingSace)og, Narrow NBSP
+      if (match === '\u00A0' || match === '\u202F') return ' '
+      return '' // Fjern alle andre tegn
+    }
   )
-  return stringUtenZeroWidthNonBreakingSace.replace(ugyldigeTegnRegex, '')
+
+  return cleanText
 }
 
 export const kanDeleDeltakerMedArrangor = (
