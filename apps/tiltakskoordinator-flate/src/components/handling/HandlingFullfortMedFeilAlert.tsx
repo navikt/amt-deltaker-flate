03c239d4f064872cc7a69f0228ac227c37f98e76
MartheFH <marthe.frogner.holm@nav.no>
2025-08-14 08:49:00 +0200

    fiks filter feil, og håndter toast for feil med skjermleser

--- apps/tiltakskoordinator-flate/src/components/handling/HandlingFullfortMedFeilAlert.tsx
+++ apps/tiltakskoordinator-flate/src/components/handling/HandlingFullfortMedFeilAlert.tsx
@@ -1,25 +1,42 @@
 import { Alert } from '@navikt/ds-react'
 import { useHandlingContext } from '../../context-providers/HandlingContext'
+import { useEffect, useState } from 'react'
 
 export const HandlingFullfortMedFeilAlert = () => {
   const { handlingFeiletText, setHandlingFeiletText } = useHandlingContext()
+  const [melding, setMelding] = useState<string | null>(null)
 
-  const onClose = () => {
-    setHandlingFeiletText(null)
-  }
+  useEffect(() => {
+    if (handlingFeiletText) {
+      setTimeout(() => {
+        // Timeout pga vi setter fokus på handling-knappen, må skjermleser kunne lese opp endringen.
+        setMelding(handlingFeiletText)
+      }, 300)
+    }
+  }, [handlingFeiletText])
 
-  if (!handlingFeiletText) return
   return (
-    <div className="sticky bottom-32 left-0 right-0 mb-20">
-      <Alert
-        closeButton
-        variant="warning"
-        size="small"
-        className="m-auto mt-4"
-        onClose={onClose}
-      >
-        {handlingFeiletText}
-      </Alert>
-    </div>
+    <>
+      <p aria-live="polite" aria-atomic={true} className="sr-only">
+        {melding ?? ''}
+      </p>
+
+      {melding ? (
+        <div className="sticky bottom-32 left-0 right-0 mb-20">
+          <Alert
+            closeButton
+            variant="warning"
+            size="small"
+            onClose={() => {
+              setMelding(null)
+              setHandlingFeiletText(null)
+            }}
+            className="m-auto mt-4"
+          >
+            {melding}
+          </Alert>
+        </div>
+      ) : null}
+    </>
   )
 }
