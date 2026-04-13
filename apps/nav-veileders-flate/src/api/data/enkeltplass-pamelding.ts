47dfa194854b7908abd83f527efc4b2c2ed984e6
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-04-13 11:08:32 +0200

    Revert "Update apps/nav-veileders-flate/src/api/data/enkeltplass-pamelding.ts"
    This reverts commit 7d79ef6afe68adce099e2e62332e14b7ca0ecc8f.

--- apps/nav-veileders-flate/src/api/data/enkeltplass-pamelding.ts
+++ apps/nav-veileders-flate/src/api/data/enkeltplass-pamelding.ts
@@ -3,8 +3,8 @@ import { z } from 'zod'
 export const enkeltplassPameldingSchema = z.object({
   beskrivelse: z.string(),
   prisinformasjon: z.string(),
-  startdato: z.string().min(1).regex(/^\d{4}-\d{2}-\d{2}$/),
-  sluttdato: z.string().min(1).regex(/^\d{4}-\d{2}-\d{2}$/),
+  startdato: z.string(),
+  sluttdato: z.string(),
   arrangorUnderenhet: z.string()
 })
 
