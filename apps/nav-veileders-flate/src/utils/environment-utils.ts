42900ea5e5a9b7001e0910b21e9ef50e576a9587
Jarle Fagerheim <jarle.fagerheim@nav.no>
2026-06-03 14:53:22 +0200

    fjern unødvendig endring

--- apps/nav-veileders-flate/src/utils/environment-utils.ts
+++ apps/nav-veileders-flate/src/utils/environment-utils.ts
@@ -1,7 +1,6 @@
 export const isPrEnv = import.meta.env.MODE === 'pull-request'
 export const useMock = ['development', 'demo'].includes(import.meta.env.MODE)
-export const isOffline = import.meta.env.MODE === 'offline'
-export const isEnvLocalDemoOrPr = useMock || isPrEnv || isOffline
+export const isEnvLocalDemoOrPr = useMock || isPrEnv
 
 export const DIALOG_URL = import.meta.env.VITE_DIALOG_URL
 export const API_URL = `${import.meta.env.BASE_URL}amt-deltaker-bff`
