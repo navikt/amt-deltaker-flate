609b8631e685c7623eafd9a33d031a1eff360ae9
Jarle Fagerheim <jarle.fagerheim@nav.no>
2026-07-02 15:55:22 +0200

    tester

--- packages/deltaker-flate-common/utils/displayText.test.ts
+++ packages/deltaker-flate-common/utils/displayText.test.ts
@@ -2,25 +2,65 @@ import { describe, expect, it } from 'vitest'
 import { deltakerprosentText } from './displayText'
 
 describe('deltakerprosentText', () => {
-  it('enkeltplass uten dager gir tom tekst', () => {
-    expect(deltakerprosentText(null, null, true)).toBe('')
+  it.each([
+    {
+      navn: 'enkeltplass uten dager',
+      deltakelsesprosent: null,
+      dagerPerUke: null,
+      erEnkeltplass: true,
+      expected: ''
+    },
+    {
+      navn: 'enkeltplass med 1 dag',
+      deltakelsesprosent: null,
+      dagerPerUke: 1,
+      erEnkeltplass: true,
+      expected: '1 dag i uka'
+    },
+    {
+      navn: 'enkeltplass med flere dager',
+      deltakelsesprosent: null,
+      dagerPerUke: 3,
+      erEnkeltplass: true,
+      expected: '3 dager i uka'
+    },
+    {
+      navn: 'gruppe uten dager',
+      deltakelsesprosent: 80,
+      dagerPerUke: null,
+      erEnkeltplass: false,
+      expected: '80\u00A0%'
+    },
+    {
+      navn: 'gruppe med prosent og dager',
+      deltakelsesprosent: 50,
+      dagerPerUke: 3,
+      erEnkeltplass: false,
+      expected: '50\u00A0% fordelt på 3 dager i uka'
+    },
+    {
+      navn: 'gruppe med nullverdi for prosent bruker 100 som fallback',
+      deltakelsesprosent: null,
+      dagerPerUke: 2,
+      erEnkeltplass: false,
+      expected: '100\u00A0% fordelt på 2 dager i uka'
+    }
+  ])(
+    '$navn',
+    ({ deltakelsesprosent, dagerPerUke, erEnkeltplass, expected }) => {
+      expect(
+        deltakerprosentText(deltakelsesprosent, dagerPerUke, erEnkeltplass)
+      ).toBe(expected)
+    }
+  )
+
+  it('har aldri trailing mellomrom for vanlige gruppe-verdier', () => {
+    expect(deltakerprosentText(80, null, false)).not.toMatch(/\s$/)
+    expect(deltakerprosentText(80, 3, false)).not.toMatch(/\s$/)
   })
 
-  it('gruppe uten dager har ikke trailing space', () => {
-    expect(deltakerprosentText(80, null, false)).toBe('80\u00A0%')
-  })
-
-  it('enkeltplass med 1 dag', () => {
-    expect(deltakerprosentText(null, 1, true)).toBe('1 dag i uka')
-  })
-
-  it('enkeltplass med flere dager', () => {
-    expect(deltakerprosentText(null, 3, true)).toBe('3 dager i uka')
-  })
-
-  it('gruppe med prosent og dager', () => {
-    expect(deltakerprosentText(50, 3, false)).toBe(
-      '50\u00A0% fordelt på 3 dager i uka'
-    )
+  it('0 dager behandles som manglende dager', () => {
+    expect(deltakerprosentText(80, 0, false)).toBe('80\u00A0%')
+    expect(deltakerprosentText(null, 0, true)).toBe('')
   })
 })
